49d1ca50889ebaf35bbf0ff77f6f3a5d6aff5524
Juan David Sierra <judas3991@gmail.com>
2025-05-07 10:03:41 -0500

    remove comments

--- infrastructure/bunny_request_service/bunny_request_service.go
+++ infrastructure/bunny_request_service/bunny_request_service.go
@@ -28,50 +28,7 @@ func (s *BunnyRequestService) GetHeaderApiKey() jsierralibs.HeaderRequest {
 }
 
 /*
-	CREATE VIDEO
-		libraryId == RESPONSE OF VIDEO LIBRARY ID
-		POST /library/{libraryId}/videos
-		AUTHORIZATION == RESPOONSE OF VIDEO LIBRARY API KEY
-		BODY => json {
-			"title": "My Video",
-		}
-		RESPONSE => json {
-			"videLibraryId": 123456,
-			"guid": "12345678-1234-1234-1234-123456789012",
-			"title": "My Video",
-			"dateUploaded": "2023-10-01T12:00:00Z",
-			"views": 0,
-			"isPublic": false,
-			"length": 0,
-			"status": "Processing",
-			"framerate": 0,
-			"width": 0,
-			"height": 0,
-			"availableResolutions": null,
-			"thumbnailCount": 0,
-			"encodedProgress": 0,
-			"storageSize": 0,
-			"captions": [],
-			"hasMP4Fallback": false,
-			"CollectionId": 0,
-		}
 
-
-
-
-	https://video.bunnycdn.com
-	GET VIDEOS BY LIBRARY ID
-		libraryId == RESPONSE OF VIDEO LIBRARY ID
-		GET /library/{libraryId}/videos
-		AUTHORIZATION == RESPOONSE OF VIDEO LIBRARY API KEY
-		RESPONSE => json {
-			"totalItems": 1,
-			"currentPage": 1,
-			"itemsPerPage": 100,
-			"items": [
-				RESPONSE OF VIDEO
-			]
-		}
 	ADD THUMBNAIL
 		libraryId == RESPONSE OF VIDEO LIBRARY ID
 		POST /library/{libraryId}/videos/{videoGuid}/thumbnails?thumbnailUrl={thumbnail_url}
@@ -98,25 +55,5 @@ func (s *BunnyRequestService) GetHeaderApiKey() jsierralibs.HeaderRequest {
 			"message": "Thumbnail added successfully",
 			"statusCode": 200,
 		}
-	UPLOAD VIDEO
-		libraryId == RESPONSE OF VIDEO LIBRARY ID
-		PUT /library/{libraryId}/videos/{videoGuid}
-		AUTHORIZATION == RESPOONSE OF VIDEO LIBRARY API KEY
-		BINARY BY BODY => file
-
-
-
-	https://vz-580df404-944.b-cdn.net/13daffd7-e78d-4936-8521-77740e2cc734/playlist.m3u8
-
-	<div style="position:relative;padding-top:56.25%;">
-		<iframe
-			src="https://iframe.mediadelivery.net/embed/421874/13daffd7-e78d-4936-8521-77740e2cc734?autoplay=true&loop=false&muted=false&preload=true&responsive=true"
-				loading="lazy"
-				style="border:0;position:absolute;top:0;height:100%;width:100%;"
-				allow="accelerometer;gyroscope;autoplay;encrypted-media;picture-in-picture;"
-				allowfullscreen="true">
-		</iframe>
-	</div>
-
 
 */
